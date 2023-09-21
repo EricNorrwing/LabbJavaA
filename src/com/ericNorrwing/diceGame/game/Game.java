2b53aa60fced159afc81c5d23d669560a129b204
EricNorrwing <eric.norrwing@gmail.com>
2023-09-21 14:33:46 +0200

    v0.2

--- src/com/ericNorrwing/diceGame/game/Game.java
+++ src/com/ericNorrwing/diceGame/game/Game.java
@@ -1,26 +1,29 @@
 package com.ericNorrwing.diceGame.game;
 
 import com.ericNorrwing.diceGame.menu.Menu;
-import com.ericNorrwing.diceGame.playerFunctions.Player;
 import com.ericNorrwing.diceGame.scanner.InputScanner;
+import com.ericNorrwing.diceGame.playerFunctions.Player;
+import java.util.ArrayList;
+
 
 public class Game {
-
     //Welcome to the main game
     public static void playGame(){
         Menu menu = new Menu();
         Game game = new Game();
-        System.out.println("Welcome to the game!");
-        int choice = menu.menu();
 
+        System.out.println("Welcome to the game!");
+        //Runs the input put by the player, new game/highscore/exit
+        menu.menu();
 
     }
 
-    public void newGame(){
+    public static void newGame(){
         System.out.println("Hello, how many would like to play? ");
         InputScanner scanner = new InputScanner();
         for (int i = 0; i < scanner.scanInt()+1; i++ ){
-            Player player = new Player(scanner.scanString(), 0);
+            Player player = new Player(scanner.scanString(), 0,0,0);
+            player.addPlayer(scanner.scanInt());
 
             System.out.println("Player added");
         }
--- src/com/ericNorrwing/diceGame/playerFunctions/Player.java
+++ src/com/ericNorrwing/diceGame/playerFunctions/Player.java
@@ -10,16 +10,19 @@ public class Player {
     int turnsPlayed;
 
 
-    public Player(String name, int score) {
+    public Player(String name, int score, int id, int turnsPlayed) {
         this.name = name;
         this.score = score;
+        this.id = id;
+        this.turnsPlayed = turnsPlayed;
     }
 
-    public void addPlayers(int inputPlayerAmount){
+    public void addPlayer(int inputPlayerAmount){
         for (int i = 0; i < inputPlayerAmount; i++){
             System.out.println("Hello please insert a name for player: " + (i+1));
             InputScanner scanner = new InputScanner();
 
         }
     }
+
 }
