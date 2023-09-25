56f0eb530a33a1b6c500c09b98481083fbe70c48
EricNorrwing <eric.norrwing@gmail.com>
2023-09-25 14:45:20 +0200

    updates to dice array functions

--- src/com/ericNorrwing/diceGame/Game.java
+++ src/com/ericNorrwing/diceGame/Game.java
@@ -9,25 +9,28 @@ public class Game {
     //initialize some stuff i might move them later?
 
     List<Player> playerList = new ArrayList<>();
-    int[] diceList = {0,0,0,0,0};
+    int[] diceList = {0,0,0,0,0,0};
     InputScanner scanner = new InputScanner();
-
+    final int ARRAY_LIST_REROLL = -1;
 
     //Welcome to the main game
     public void playGame(){
-
+        System.out.println("Welcome to the menu: Please make a choice: ");
+        System.out.println("1. New Game \n2. Check highscore \n3. Exit game");
 
         //Game game = new Game();
 
         // new game();
         // highScore();
         // Exit();
-        switch (menu()) {
+        switch (scanner.scanInt()) {
             case 1:
                 newGame();
                 runGame();
-
+                break;
             case 2:
+                testAllTheStuff();
+                break;
                 //getHighscore();
             case 3:
                 //exitGame();
@@ -53,8 +56,63 @@ public class Game {
             Player currentPlayer = playerList.get(i);
             System.out.println("Heres your dice " + currentPlayer.getName());
             printCurrentDice();
+            checkScore();
+            if (checkIfDiceScored()){
+                System.out.println("Which dice would you like to keep? Current score is : ");
+            } else {
+                System.out.println("You didnt score any points");
+            }
+
         }
         return 0;
+    }
+    private int checkScore(){
+        checkMultiples();
+        //checkOnes();
+        //checkFives();
+        return 0;
+    }
+    private int checkMultiples(){
+        int score = 0;
+        int dupeCounter = 0;
+        int diceDupeValue = 0;
+            for (int i = 0; i < diceList.length; i++) {
+                int firstDiceCheck = diceList[i];
+                for (int j = 0; j < diceList.length; j++) {
+                    if (i == j) {
+                        continue;
+                    }else{
+                        int secondDiceCheck = diceList[j];
+                        if (firstDiceCheck == secondDiceCheck) {
+                            System.out.println("youre in loop " + i);
+                            diceDupeValue = firstDiceCheck;
+                            dupeCounter++;
+                    }
+
+
+                    }
+                }
+            }
+        if(dupeCounter >= 3){
+            System.out.println(dupeCounter);
+            System.out.println("Dice dupe value is " + diceDupeValue);
+        }
+        return score;
+    }
+    private boolean checkIfDiceScored(){
+        for (int i = 0; i < diceList.length; i++){
+            if(diceList[i] == 1 || diceList[i] == 5){
+                System.out.println("You scored something");
+                return true;
+            }
+            else{
+                System.out.println("You didnt score");
+                return false;
+
+            }
+        }
+        System.out.println("this is an error code");
+        return false;
     }
     public void newGame(){
         System.out.println("Hello, how many would like to play?");
@@ -70,10 +128,6 @@ public class Game {
 
 
         }
-        //remove later, prints the players
-        System.out.println(playerList.toString());
-
-
     }
 
     //Returns 1-6
@@ -93,14 +147,13 @@ public class Game {
         for(int i: diceList) {
             System.out.print(i + " ");
         }
+        System.out.println("\n");
     }
-
-    public int menu(){
-        System.out.println("Welcome to the menu: Please make a choice: ");
-        System.out.println("1. New Game \n2. Check highscore \n3. Exit game");
-
-        InputScanner scanner = new InputScanner();
-        return scanner.scanInt();
+    private void testAllTheStuff(){
+        rollNewRound();
+        printCurrentDice();
+        checkScore();
+        System.out.println("lmao testing shit");
     }
 
 }
